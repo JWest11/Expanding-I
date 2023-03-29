b5c1a9d327ecbb3a173eab5eaa7fbda2fc9f8315
JWest11 <114183436+JWest11@users.noreply.github.com>
2023-03-29 14:01:15 -0600

    new stuff

--- iExpanding.js
+++ iExpanding.js
@@ -1,17 +1,19 @@
+let ihovered = false;
+
 function activateIExpansion() {
     const iElement = document.getElementById("i-container");
     const iRect = iElement.getBoundingClientRect();
-    var deltaX = iRect.x + iRect.width / 1.8;
-    var deltaY = iRect.y + iRect.height / 4;
-    document.documentElement.style.setProperty("--dot-x-offset", deltaX -2 + "px");
-    document.documentElement.style.setProperty("--dot-y-offset", deltaY -2 + "px");
+    let deltaX = iRect.x + iRect.width / 1.8;
+    let deltaY = iRect.y + iRect.height / 4;
+    document.documentElement.style.setProperty("--dot-x-offset", deltaX + "px");
+    document.documentElement.style.setProperty("--dot-y-offset", deltaY + "px");
 
     const expandedContentElement = document.getElementById("expanded-content");
     
     setTimeout(() => {
         expandedContentElement.dataset.delayedtransition = "true";
         expandedContentElement.dataset.visible = "true";
-    })
+    }, 100)
     
 }
 
@@ -19,26 +21,29 @@ function deactivateIExpansion() {
     const expandedContentElement = document.getElementById("expanded-content");
     setTimeout(() => {
         expandedContentElement.dataset.visible = "false";
-    })
+    }, 100)
 }
 
 function peekIExpansion() {
+    ihovered = true;
     const expandedContentElement = document.getElementById("expanded-content");
     expandedContentElement.dataset.delayedtransition = "false";
     const iElement = document.getElementById("i-container");
     const iRect = iElement.getBoundingClientRect();
-    var deltaX = iRect.x + iRect.width / 1.8;
-    var deltaY = iRect.y + iRect.height / 4;
-    document.documentElement.style.setProperty("--dot-x-offset", deltaX -1 + "px");
-    document.documentElement.style.setProperty("--dot-y-offset", deltaY -2 + "px");
+    let deltaX = iRect.x + iRect.width / 1.8;
+    let deltaY = iRect.y + iRect.height / 4;
+    document.documentElement.style.setProperty("--dot-x-offset", deltaX + "px");
+    document.documentElement.style.setProperty("--dot-y-offset", deltaY + "px");
     
     setTimeout(() => {
+        if (!ihovered) {return}
         expandedContentElement.dataset.delayedtransition = "fast";
         expandedContentElement.dataset.peek = "true";
-    })
+    }, 100)
 }
 
 function unPeekIExpansion() {
+    ihovered = false;
     const expandedContentElement = document.getElementById("expanded-content");
     expandedContentElement.dataset.peek = "false";
 }
